36a5218ffbdc7e75f1165a93d80bd4e997b7c786
ASZhartun <as.zhartun@gmail.com>
2022-12-08 17:03:36 +0200

    Add class for zipping folder

---
+++ src/ZipDoer.java
@@ -0,0 +1,98 @@
+import java.io.*;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
+
+public class ZipDoer {
+    /**
+     * <p>Demonstration: how it works.</p>
+     * <b>Params</b>:
+     * <ul>
+     *     <li>pathname - your folder for zipping</li>
+     *     <li>destiny - your zip of folder</li>
+     * </ul>
+     * <p>Use getZOS for getting stream with zip and use base `pack` method for zipping.</p>
+     *
+     * @param args
+     */
+    public static void main(String[] args) {
+        final String pathname = "E:\\projects\\javafx-core\\ZipZap\\files\\poiu";
+        final File source = new File(pathname);
+        final File destiny = new File(pathname + ".zip");
+
+        try {
+            final ZipOutputStream zos = getZipOutputStream(destiny);
+            pack(source, zos, null);
+            zos.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    /**
+     * Get stream with your zip file.
+     *
+     * @param destiny zip file
+     * @return ZipOutputStream for base method in next usage.
+     * @throws FileNotFoundException trouble with file system
+     */
+    private static ZipOutputStream getZipOutputStream(File destiny) throws FileNotFoundException {
+        final FileOutputStream fos = new FileOutputStream(destiny);
+        return new ZipOutputStream(fos);
+    }
+
+    /**
+     * Check folder or file it is. And pack by utilize method. If source is folder method call itself recursively
+     * with updated pathname.
+     * @param sourceFolder file we want to zip
+     * @param zos ZipOutputStream for base method in next usage.
+     * @param prevPath path of parent directory
+     * @throws IOException
+     */
+    private static void pack(File sourceFolder, ZipOutputStream zos, String prevPath) throws IOException {
+        String parentDir = "";
+        if (prevPath != null) {
+            parentDir = prevPath + "\\";
+        }
+        if (sourceFolder.isFile()) {
+            packFile(sourceFolder, zos, parentDir);
+        } else {
+            final File[] files = sourceFolder.listFiles();
+            for (File next :
+                    files) {
+                pack(next, zos, parentDir + sourceFolder.getName());
+            }
+        }
+    }
+
+    /**
+     * Pack source file to zip.
+     * @param source source file
+     * @param zos ZipOutputStream for base method in next usage.
+     * @param prevPath path of parent directory
+     * @throws IOException
+     */
+    public static void packFile(File source, ZipOutputStream zos, String prevPath) throws IOException {
+        String parentDir = "";
+        if (prevPath != null) {
+            parentDir = prevPath;
+        }
+        final ZipEntry zipEntry = new ZipEntry(parentDir + source.getName());
+
+        zos.putNextEntry(zipEntry);
+
+        final FileInputStream fis = new FileInputStream(source);
+
+        byte[] buffer = new byte[1024];
+        int byteQuantity;
+
+        while ((byteQuantity = fis.read(buffer)) > 0) {
+            zos.write(buffer, 0, byteQuantity);
+        }
+
+        zos.closeEntry();
+        fis.close();
+
+        System.out.println(source.getName() + " was zipped.");
+    }
+}
